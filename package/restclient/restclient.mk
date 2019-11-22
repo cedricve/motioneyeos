415e6f3c015a68fa018132b05830efa1bc0d2b39
Cedric Verstraeten <cedric@verstraeten.io>
2019-11-22 22:50:36 +0100

    fox

--- package/restclient/restclient.mk
+++ package/restclient/restclient.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RESTCLIENT_VERSION = master
+RESTCLIENT_VERSION = b9ac3c8a8487927717596ac492eaa1366e152e87
 RESTCLIENT_SITE = https://github.com/cedricve/restclient-cpp
 RESTCLIENT_SITE_METHOD = git
 RESTCLIENT_INSTALL_TARGET = YES
