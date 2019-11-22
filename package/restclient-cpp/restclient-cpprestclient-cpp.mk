a86167d11f1a8408ea62aed1fd704f5bcd35eebf
Cedric Verstraeten <cedric@verstraeten.io>
2019-11-22 22:44:37 +0100

    fix

--- package/restclient-cpp/restclient-cpprestclient-cpp.mk
+++ package/restclient-cpp/restclient-cpprestclient-cpp.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 RESTCLIENT_VERSION = master
-RESTCLIENT_MACHINERY_SITE = https://github.com/cedricve/restclient-cpp
-RESTCLIENT_MACHINERY_SITE_METHOD = git
-RESTCLIENT_MACHINERY_INSTALL_TARGET = YES
+RESTCLIENT_SITE = https://github.com/cedricve/restclient-cpp
+RESTCLIENT_SITE_METHOD = git
+RESTCLIENT_INSTALL_TARGET = YES
 
 $(eval $(autotools-package))
