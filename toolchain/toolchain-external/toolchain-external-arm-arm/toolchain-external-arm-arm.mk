b04b8aceb16fb5569965b0c43914be52ed26bff1
Cedric Verstraeten <cedric@verstraeten.io>
2019-11-27 16:38:56 +0100

    fix

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -8,7 +8,7 @@
 #TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)
 #TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/5_4-2016q2
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm
 TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gccarmnoneeabi542016q220160622srctar.bz2
 
 $(eval $(toolchain-external-package))
