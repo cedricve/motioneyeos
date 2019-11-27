4e1c9a71e5317306a1816bcadf8b88d4b605eccd
Cedric Verstraeten <cedric@verstraeten.io>
2019-11-27 19:22:30 +0100

    fix

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -8,7 +8,8 @@
 #TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)
 #TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm
-TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gccarmnoneeabi532016q120160330srctar.bz2
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2017.02
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://releases.linaro.org/components/toolchain/binaries/6.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/arm-linux-gnueabihf
+TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-linaro-6.3.1-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64_arm-linux-gnueabihf.tar.xz
 
 $(eval $(toolchain-external-package))
