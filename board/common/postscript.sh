c8ccaa5015d8cbf6482952f55df4fd03bc0660bf
Cedric Verstraeten <cedric@verstraeten.io>
2019-11-29 16:41:16 +0100

    fix

--- board/common/postscript.sh
+++ board/common/postscript.sh
@@ -31,7 +31,7 @@ rm -rf ${TARGET}/var/spool
 rm -rf ${TARGET}/var/tmp
 
 ln -s /tmp ${TARGET}/var/cache
-#ln -s /data/varlib ${TARGET}/var/lib
+ln -s /data/varlib ${TARGET}/var/lib
 ln -s /tmp ${TARGET}/var/lock
 ln -s /data/log ${TARGET}/var/log
 ln -s /tmp ${TARGET}/var/run
